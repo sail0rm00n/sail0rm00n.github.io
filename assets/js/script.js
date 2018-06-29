3ad063bc28897fdd90084d9d530a9e205648ee89
sail0rm00n <nicoledyoon@gmail.com>
2018-06-28 23:34:21 -0400

    overlay menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,5 @@
 // shorthand for document.ready
 $(function() {
-    // hover accepts two functions
-    // 1st function fires when mouse is in state of hover
-    // $('ul.parent > li').hover(function() {
-    //     // $('this').find('ul.child').stop().show(200);
-    //     $('this').find('ul.child').css('display', 'block');
-    //     console.log("Hi")
-    // // 2nd function fires when mouse exits hover state
-    // }, function() {
-    //     $('this').find('ul.child').stop().hide(200);
-    // });
-    
-    // $( "ul.parent > li" ).hover(function() {
-    //       $(this).find("ul.child").addClass("hover");
-    //       console.log("hello")
-    //     }, function() {
-    //       $(this).find("ul.child").removeClass("hover" );
-    //     }
-    // );
 
     $( ".work" ).hover(function() {
         $(this).find("ul.child").addClass("hover");
@@ -34,15 +16,16 @@ $(function() {
         window.location.href = "./contact.html";
     })
 
-    // hamburger icon
-    // $('.icon').click(function(){
-	// 	$(this).toggleClass('open');
-    // });
-
     $('#toggle').click(function() {
         $(this).toggleClass('toggle-active');
         $('#overlay').toggleClass('nav-active');
     });
 });
+
+
+    // hamburger icon
+    // $('.icon').click(function(){
+	// 	$(this).toggleClass('open');
+    // });
 
 
